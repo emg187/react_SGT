5030266f0496c02231f0f0752eaf697d4a17c5cd
Eric George <geric738@aol.com>
2019-03-27 12:54:34 -0700

    completed displaying dummy student data

---
+++ src/dummy_data/student_list.js
@@ -0,0 +1,33 @@
+export default [
+    {
+        name: "Clark Kent", 
+        course: "Flying 101", 
+        grade: "98", 
+        id: 1
+    }, 
+    {
+        name: "Eric George", 
+        course: "Math", 
+        grade: "82", 
+        id: 2
+    }, 
+    {
+        name: "Jack Dawson", 
+        course: "Ice Dodging", 
+        grade: "52", 
+        id: 3
+    }, 
+    {
+        name: "Jason Yata", 
+        course: "Hustling", 
+        grade: "100", 
+        id: 4
+    }, 
+    {
+        name: "Dan Paschal", 
+        course: "Computer Science", 
+        grade: "100", 
+        id: 5
+    }
+];
+
